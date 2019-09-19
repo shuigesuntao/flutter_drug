560bc963b6b2049664f8eee9a6df6cdde993acda
tom <2772502640@qq.com>
2019-09-19 16:48:12 +0800

    http优化

--- lib/main.dart
+++ lib/main.dart
@@ -7,7 +7,6 @@ import 'package:oktoast/oktoast.dart';
 import 'package:provider/provider.dart';
 
 import 'config/router_config.dart';
-import 'ui/page/splash.dart';
 
 void main() async {
   Provider.debugCheckInvalidValueType = null;
@@ -21,39 +20,25 @@ void main() async {
 }
 
 class App extends StatelessWidget {
-  /// 在App运行之前,需要初始化的异步操作
-  /// 如果存在多个,可以使用[Future.wait(futures)]来合并future后传入
-  final Future future;
-
-  const App({this.future});
 
   @override
   Widget build(BuildContext context) {
-    return FutureBuilder(
-      future: future,
-      builder: (context, snapshot){
-        /// 在异步操作时,显示的页面
-        if (snapshot.connectionState != ConnectionState.done) {
-          return SplashImage();
-        }
-        return OKToast(
-          child: MultiProvider(
-            providers: providers,
-            child: MaterialApp(
-              debugShowCheckedModeBanner: false,
-              title: '药匣子',
-              theme: ThemeData(
-                primaryColor: Color(0xFF25bbaf),
-                scaffoldBackgroundColor: Color(0xFFF2F2F2),
-                primaryIconTheme: IconThemeData(color: Colors.white),
-                appBarTheme: AppBarTheme(elevation: 0)
-              ),
-              onGenerateRoute: Router.generateRoute,
-              initialRoute: RouteName.splash,
-            ),
-          )
-        );
-      },
+    return OKToast(
+      child: MultiProvider(
+        providers: providers,
+        child: MaterialApp(
+          debugShowCheckedModeBanner: false,
+          title: '药匣子',
+          theme: ThemeData(
+            primaryColor: Color(0xFF25bbaf),
+            scaffoldBackgroundColor: Color(0xFFF2F2F2),
+            primaryIconTheme: IconThemeData(color: Colors.white),
+            appBarTheme: AppBarTheme(elevation: 0)
+          ),
+          onGenerateRoute: Router.generateRoute,
+          initialRoute: RouteName.splash,
+        ),
+      )
     );
 
   }
--- lib/ui/page/splash.dart
+++ lib/ui/page/splash.dart
@@ -5,23 +5,6 @@ import 'package:flutter_drug/config/resource_mananger.dart';
 import 'package:flutter_drug/config/router_config.dart';
 
 
-
-/// 用于项目初始化之前显示的页面
-class SplashImage extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      color: Colors.white,//背景色
-      width: double.infinity,
-      height: double.infinity,
-      child: Image.asset(
-        ImageHelper.wrapAssets('splash_bg.jpg'),
-      ),
-    );
-  }
-}
-
-
 class SplashPage extends StatefulWidget {
   @override
   _SplashPageState createState() => _SplashPageState();
@@ -32,16 +15,7 @@ class _SplashPageState extends State<SplashPage> with TickerProviderStateMixin {
   @override
   void initState() {
     super.initState();
-    countDown();
-  }
-
-  void countDown() {
-    var _duration = new Duration(seconds: 3);
-    new Future.delayed(_duration, nextPage);
-  }
-
-  void nextPage() {
-    Navigator.of(context).pushReplacementNamed(RouteName.tab);
+    Future.delayed(Duration(seconds: 3), () => Navigator.of(context).pushReplacementNamed(RouteName.tab));
   }
 
   @override
