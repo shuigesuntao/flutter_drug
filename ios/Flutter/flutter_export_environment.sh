6b6b62d178cb3e85301327d32332df488b9ee099
tom <2772502640@qq.com>
2020-04-27 09:56:30 +0800

    升级依赖库

--- ios/Flutter/flutter_export_environment.sh
+++ ios/Flutter/flutter_export_environment.sh
@@ -1,11 +1,10 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=/Users/sun/flutter"
+export "FLUTTER_ROOT=/Users/sun/Library/flutter"
 export "FLUTTER_APPLICATION_PATH=/Users/sun/AndroidStudioProjects/flutter_drug/flutter_drug"
-export "FLUTTER_TARGET=/Users/sun/AndroidStudioProjects/flutter_drug/flutter_drug/lib/main.dart"
+export "FLUTTER_TARGET=lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
 export "SYMROOT=${SOURCE_ROOT}/../build/ios"
-export "FLUTTER_FRAMEWORK_DIR=/Users/sun/flutter/bin/cache/artifacts/engine/ios"
+export "FLUTTER_FRAMEWORK_DIR=/Users/sun/Library/flutter/bin/cache/artifacts/engine/ios"
 export "FLUTTER_BUILD_NAME=5.0.3"
 export "FLUTTER_BUILD_NUMBER=3"
-export "TRACK_WIDGET_CREATION=true"
